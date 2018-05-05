7873d5d31f8516dd1f0a267b94ea0e3b20ea3e72
Benedikt Wakonig <benediktwakonig@Benedikt-Wakonigs-MacBook-Pro.local>
2018-05-05 21:33:17 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,69 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {NgxAutoScrollModule} from "ngx-auto-scroll";
+
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/index';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_directives/index';
+import { AuthGuard } from './_guards/index';
+import { JwtInterceptor } from './_helpers/index';
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+import { TimeAgoPipe } from 'time-ago-pipe';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        routing,
+        NgxAutoScrollModule,
+        BrowserAnimationsModule,
+        AngularFontAwesomeModule,
+        MatInputModule,
+        MatCardModule
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        HeaderComponent,
+        FooterComponent,
+        TimeAgoPipe
+    ],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: JwtInterceptor,
+            multi: true
+        },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+﻿import { Component, OnInit, Input } from '@angular/core';
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+
+
+@Component({
+    moduleId: module.id.toString(),
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+
+    currentUser: User;
+    users: User[] = [];
+    heroes = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    addHero(newHero: string, date: Date) {
+
+        if (newHero) {
+
+            this.heroes.push({date: new Date().toLocaleString(),message: newHero});
+    
+        }
+        
+      }
+    
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+}
